54707b9da742c6444abde0a387e04b58d5dc23b4
Oblivion サン <OblivionSan@users.noreply.github.com>
2017-11-25 19:45:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const Discord = require("discord.js");
+const Discord = require(`discord.js`);
 const client = new Discord.Client();
-const fs = require("fs");
-const config = require("./config.json");
+const fs = require(`fs`);
+const config = require(`./config.json`);
 
-fs.readdir("./events/", (err, files) => {
+fs.readdir(`./events/`, (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     let eventFunction = require(`./events/${file}`);
